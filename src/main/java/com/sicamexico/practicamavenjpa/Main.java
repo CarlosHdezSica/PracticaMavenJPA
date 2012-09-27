e6bbbf69ad0c539abe3b5eb6db0445bbae778a73
CarlosHdezSica <carlos.hernandez.sica@gmail.com>
2012-09-27 17:45:37 -0500

    Se agrega el esqueleto para prácticas de JPA habilitado para Maven.

---
+++ src/main/java/com/sicamexico/practicamavenjpa/Main.java
@@ -0,0 +1,13 @@
+package com.sicamexico.practicamavenjpa;
+
+/**
+ * Hello world!
+ *
+ */
+public class Main 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
